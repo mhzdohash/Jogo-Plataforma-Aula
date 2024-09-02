8024e1d74a673217496967bb054320fdebbde508
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-02 13:31:06 -0300

    Jogo2D

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+     private Rigidbody2D rb;
+    private float horizontal;
+    private bool isFacingRight = true;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        Debug.Log(horizontal);
+         
+        this.rb.velocity = new Vector2(horizontal * 8f, this.rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.rb.AddForce(Vector2.up * 14f, ForceMode2D.Impulse);
+        }
+       
+        Flip();
+            
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localscale = transform.localScale;
+            localscale.x *= -1;
+            transform.localScale = localscale; 
+
+        }
+    }
+}
